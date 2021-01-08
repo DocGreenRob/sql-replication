15fa1dee085718c0d2420d51aa67fc5704283109
Robert Green <robert.green.it.2017@gmail.com>
2021-01-08 12:52:51 -0500

    wip

--- SqlReplication/sqlR.cs
+++ SqlReplication/sqlR.cs
@@ -8,205 +8,203 @@ using System.Collections.Generic;
 
 namespace SqlReplication
 {
+	class sqlR
+	{
+		static void Main(string[] args)
+		{
+			string connectionString = System.Configuration.ConfigurationManager.ConnectionStrings["Local"].ConnectionString;
 
-    class sqlR
-    {
+			if (args.Contains("-action"))
+			{
+				//Distribution File
+				if (args[1].ToString().Equals("distribution"))
+				{
+					try
+					{
+						string pathDistributionFile = ConfigurationManager.AppSettings["DistributionFile"];
 
-        static void Main(string[] args)
-        {
-            string connectionString = System.Configuration.ConfigurationManager.ConnectionStrings["Local"].ConnectionString;
+						string script = File.ReadAllText(pathDistributionFile);
 
-            if (args.Contains("-action")) 
-            {
-                //Distribution File
-                if (args[1].ToString().Equals("distribution"))
-                {
-                    try
-                    {
-                        string pathDistributionFile = ConfigurationManager.AppSettings["DistributionFile"];
+						// split script on GO   command
+						System.Collections.Generic.IEnumerable<string> commandStrings = Regex.Split(script, @"^\s*GO\s*$",
+												 RegexOptions.Multiline | RegexOptions.IgnoreCase);
+						using (SqlConnection connection = new SqlConnection(connectionString))
+						{
+							connection.Open();
+							foreach (string commandString in commandStrings)
+							{
+								if (commandString.Trim() != "")
+								{
+									using (var command = new SqlCommand(commandString, connection))
+									{
+										try
+										{
+											command.ExecuteNonQuery();
+										}
+										catch (SqlException ex)
+										{
+											string spError = commandString.Length > 100 ? commandString.Substring(0, 100) + " ...\n..." : commandString;
+											Console.WriteLine(string.Format("Please check the SqlServer script.\nFile: {0} \nLine: {1} \nError: {2} \nSQL Command: \n{3}", pathDistributionFile, ex.LineNumber, ex.Message, spError));
 
-                        string script = File.ReadAllText(pathDistributionFile);
+										}
+									}
+								}
+							}
+							connection.Close();
+						}
 
-                        // split script on GO command
-                        System.Collections.Generic.IEnumerable<string> commandStrings = Regex.Split(script, @"^\s*GO\s*$",
-                                                 RegexOptions.Multiline | RegexOptions.IgnoreCase);
-                        using (SqlConnection connection = new SqlConnection(connectionString))
-                        {
-                            connection.Open();
-                            foreach (string commandString in commandStrings)
-                            {
-                                if (commandString.Trim() != "")
-                                {
-                                    using (var command = new SqlCommand(commandString, connection))
-                                    {
-                                        try
-                                        {
-                                            command.ExecuteNonQuery();
-                                        }
-                                        catch (SqlException ex)
-                                        {
-                                            string spError = commandString.Length > 100 ? commandString.Substring(0, 100) + " ...\n..." : commandString;
-                                            Console.WriteLine(string.Format("Please check the SqlServer script.\nFile: {0} \nLine: {1} \nError: {2} \nSQL Command: \n{3}", pathDistributionFile, ex.LineNumber, ex.Message, spError));
+					}
+					catch (Exception ex)
+					{
+						Console.WriteLine(ex.Message);
+					}
+				}
 
-                                        }
-                                    }
-                                }
-                            }
-                            connection.Close();
-                        }
+				//Publication File
+				else if (args[1].ToString().Equals("publication"))
+				{
+					try
+					{
+						string pathDistributionFile = ConfigurationManager.AppSettings["PublicationFile"];
 
-                    }
-                    catch (Exception ex)
-                    {
-                        Console.WriteLine(ex.Message);
-                    }
-                }
+						string script = File.ReadAllText(pathDistributionFile);
 
-                //Publication File
-                else if (args[1].ToString().Equals("publication"))
-                {
-                    try
-                    {
-                        string pathDistributionFile = ConfigurationManager.AppSettings["PublicationFile"];
+						//Parse Vales
+						for (int i = 0; i < args.Length; i++)
+						{
+							if (args[i] == "-database")
+							{
+								script = script.Replace("{{database_name}}", args[i + 1].ToString());
+							}
+							if (args[i] == "-replpublicationname")
+							{
+								script = script.Replace("{{publication_name}}", args[i + 1].ToString());
+							}
+							if (args[i] == "-tablenames")
+							{
+								script = script.Replace("{{tables_names}}", args[i + 1].ToString());
+							}
+							if (args[i] == "-sqlloginuser")
+							{
+								script = script.Replace("{{sql_user}}", args[i + 1].ToString());
+							}
+							if (args[i] == "-sqlloginpassword")
+							{
+								script = script.Replace("{{user_pwd}}", args[i + 1].ToString());
+							}
+						}
+						// split script on GO command
+						System.Collections.Generic.IEnumerable<string> commandStrings = Regex.Split(script, @"^\s*GO\s*$",
+												 RegexOptions.Multiline | RegexOptions.IgnoreCase);
+						using (SqlConnection connection = new SqlConnection(connectionString))
+						{
+							connection.Open();
+							foreach (string commandString in commandStrings)
+							{
+								if (commandString.Trim() != "")
+								{
+									using (var command = new SqlCommand(commandString, connection))
+									{
+										try
+										{
+											command.ExecuteNonQuery();
+										}
+										catch (SqlException ex)
+										{
+											string spError = commandString.Length > 100 ? commandString.Substring(0, 100) + " ...\n..." : commandString;
+											Console.WriteLine(string.Format("Please check the SqlServer script.\nFile: {0} \nLine: {1} \nError: {2} \nSQL Command: \n{3}", pathDistributionFile, ex.LineNumber, ex.Message, spError));
 
-                        string script = File.ReadAllText(pathDistributionFile);
+										}
+									}
+								}
+							}
+							connection.Close();
+						}
 
-                        //Parse Vales
-                        for (int i = 0; i < args.Length; i++)
-                        {
-                            if (args[i] == "-database")
-                            {
-                                script = script.Replace("{{database_name}}", args[i + 1].ToString());
-                            }
-                            if (args[i] == "-replpublicationname")
-                            {
-                                script = script.Replace("{{publication_name}}", args[i+1].ToString());
-                            }
-                            if (args[i] == "-tablenames")
-                            {
-                                script = script.Replace("{{tables_names}}", args[i + 1].ToString());
-                            }
-                            if (args[i] == "-sqlloginuser")
-                            {
-                                script = script.Replace("{{sql_user}}", args[i + 1].ToString());
-                            }
-                            if (args[i] == "-sqlloginpassword")
-                            {
-                                script = script.Replace("{{user_pwd}}", args[i + 1].ToString());
-                            }
-                        }
-                        // split script on GO command
-                        System.Collections.Generic.IEnumerable<string> commandStrings = Regex.Split(script, @"^\s*GO\s*$",
-                                                 RegexOptions.Multiline | RegexOptions.IgnoreCase);
-                        using (SqlConnection connection = new SqlConnection(connectionString))
-                        {
-                            connection.Open();
-                            foreach (string commandString in commandStrings)
-                            {
-                                if (commandString.Trim() != "")
-                                {
-                                    using (var command = new SqlCommand(commandString, connection))
-                                    {
-                                        try
-                                        {
-                                            command.ExecuteNonQuery();
-                                        }
-                                        catch (SqlException ex)
-                                        {
-                                            string spError = commandString.Length > 100 ? commandString.Substring(0, 100) + " ...\n..." : commandString;
-                                            Console.WriteLine(string.Format("Please check the SqlServer script.\nFile: {0} \nLine: {1} \nError: {2} \nSQL Command: \n{3}", pathDistributionFile, ex.LineNumber, ex.Message, spError));
+					}
+					catch (Exception ex)
+					{
+						Console.WriteLine(ex.Message);
+					}
+				}
+				//Subscription File
+				else if (args[1].ToString().Equals("subscription"))
+				{
+					try
+					{
+						string pathDistributionFile = ConfigurationManager.AppSettings["SubscriptionFile"];
 
-                                        }
-                                    }
-                                }
-                            }
-                            connection.Close();
-                        }
+						string script = File.ReadAllText(pathDistributionFile);
 
-                    }
-                    catch (Exception ex)
-                    {
-                        Console.WriteLine(ex.Message);
-                    }
-                }
-                //Subscription File
-                else if (args[1].ToString().Equals("subscription"))
-                {
-                    try
-                    {
-                        string pathDistributionFile = ConfigurationManager.AppSettings["SubscriptionFile"];
+						//Parse Vales
+						for (int i = 0; i < args.Length; i++)
+						{
+							if (args[i] == "-database")
+							{
+								script = script.Replace("{{database_name}}", args[i + 1].ToString());
+							}
+							if (args[i] == "-replpublicationname")
+							{
+								script = script.Replace("{{publication_name}}", args[i + 1].ToString());
+							}
+							if (args[i] == "-subscriber_ip")
+							{
+								script = script.Replace("{{subscriber_ip}}", args[i + 1].ToString());
+							}
+							if (args[i] == "-subscriberDB")
+							{
+								script = script.Replace("{{sub_db}}", args[i + 1].ToString());
+							}
+							if (args[i] == "-sqlloginuser")
+							{
+								script = script.Replace("{{sql_user}}", args[i + 1].ToString());
+							}
+							if (args[i] == "-sqlloginpassword")
+							{
+								script = script.Replace("{{user_pwd}}", args[i + 1].ToString());
+							}
+						}
 
-                        string script = File.ReadAllText(pathDistributionFile);
+						// split script on GO command
+						System.Collections.Generic.IEnumerable<string> commandStrings = Regex.Split(script, @"^\s*GO\s*$",
+												 RegexOptions.Multiline | RegexOptions.IgnoreCase);
+						using (SqlConnection connection = new SqlConnection(connectionString))
+						{
+							connection.Open();
+							foreach (string commandString in commandStrings)
+							{
+								if (commandString.Trim() != "")
+								{
+									using (var command = new SqlCommand(commandString, connection))
+									{
+										try
+										{
+											command.ExecuteNonQuery();
+										}
+										catch (SqlException ex)
+										{
+											string spError = commandString.Length > 100 ? commandString.Substring(0, 100) + " ...\n..." : commandString;
+											Console.WriteLine(string.Format("Please check the SqlServer script.\nFile: {0} \nLine: {1} \nError: {2} \nSQL Command: \n{3}", pathDistributionFile, ex.LineNumber, ex.Message, spError));
 
-                        //Parse Vales
-                        for (int i = 0; i < args.Length; i++)
-                        {
-                            if (args[i] == "-database")
-                            {
-                                script = script.Replace("{{database_name}}", args[i + 1].ToString());
-                            }
-                            if (args[i] == "-replpublicationname")
-                            {
-                                script = script.Replace("{{publication_name}}", args[i + 1].ToString());
-                            }
-                            if (args[i] == "-subscriber_ip")
-                            {
-                                script = script.Replace("{{subscriber_ip}}", args[i + 1].ToString());
-                            }
-                            if (args[i] == "-subscriberDB")
-                            {
-                                script = script.Replace("{{sub_db}}", args[i + 1].ToString());
-                            }
-                            if (args[i] == "-sqlloginuser")
-                            {
-                                script = script.Replace("{{sql_user}}", args[i + 1].ToString());
-                            }
-                            if (args[i] == "-sqlloginpassword")
-                            {
-                                script = script.Replace("{{user_pwd}}", args[i + 1].ToString());
-                            }
-                        }
+										}
+									}
+								}
+							}
+							connection.Close();
+						}
 
-                        // split script on GO command
-                        System.Collections.Generic.IEnumerable<string> commandStrings = Regex.Split(script, @"^\s*GO\s*$",
-                                                 RegexOptions.Multiline | RegexOptions.IgnoreCase);
-                        using (SqlConnection connection = new SqlConnection(connectionString))
-                        {
-                            connection.Open();
-                            foreach (string commandString in commandStrings)
-                            {
-                                if (commandString.Trim() != "")
-                                {
-                                    using (var command = new SqlCommand(commandString, connection))
-                                    {
-                                        try
-                                        {
-                                            command.ExecuteNonQuery();
-                                        }
-                                        catch (SqlException ex)
-                                        {
-                                            string spError = commandString.Length > 100 ? commandString.Substring(0, 100) + " ...\n..." : commandString;
-                                            Console.WriteLine(string.Format("Please check the SqlServer script.\nFile: {0} \nLine: {1} \nError: {2} \nSQL Command: \n{3}", pathDistributionFile, ex.LineNumber, ex.Message, spError));
+					}
+					catch (Exception ex)
+					{
+						Console.WriteLine(ex.Message);
+					}
+				}
+			}
+			else
+			{
+				Console.WriteLine("No action provided...");
+			}
 
-                                        }
-                                    }
-                                }
-                            }
-                            connection.Close();
-                        }
-
-                    }
-                    catch (Exception ex)
-                    {
-                        Console.WriteLine(ex.Message);
-                    }
-                }
-            }
-            else
-            {
-                Console.WriteLine("No action provided...");
-            }
-           
-        }
-    }
+		}
+	}
 }
